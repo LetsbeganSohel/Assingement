5b9cf06898ec0e49c19aacbd98260e1ede4c6a61
Md Sohel Rana <110485029+LetsbeganSohel@users.noreply.github.com>
2022-09-01 12:30:28 +0530

    Add files via upload

---
+++ ExampleOfObject.java
@@ -0,0 +1,12 @@
+class ExampleOfObject{
+public void Object1(){
+int a=7;
+int b=6;
+int c;
+c=a+b;
+System.out.println(c);
+}
+public static void main(String args[]){
+ExampleOfObject o1=new ExampleOfObject();
+o1.Object1();
+}}
